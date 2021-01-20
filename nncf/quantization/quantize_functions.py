0e40d0275d1c6f32685f38d26bca7914bd3e7c37
hussam789 <hussam789@gmail.com>
2021-01-20 14:42:34 +0200

    make sure input_low is cuda if input is cuda

--- nncf/quantization/quantize_functions.py
+++ nncf/quantization/quantize_functions.py
@@ -30,7 +30,7 @@ class QuantizeSymmetric(torch.autograd.Function):
             if not input_.is_contiguous():
                 warnings.warn("input_ is not contiguous!", RuntimeWarning)
                 input_ = input_.contiguous()
-            output = QuantizedFunctionsCUDA.Quantize_forward(input_, input_low, input_range, levels)
+            output = QuantizedFunctionsCUDA.Quantize_forward(input_, input_low.cuda(), input_range, levels)
         else:
             output = QuantizedFunctionsCPU.Quantize_forward(input_, input_low, input_range, levels)
 
@@ -71,7 +71,7 @@ class QuantizeAsymmetric(torch.autograd.Function):
             if not input_.is_contiguous():
                 warnings.warn("input_ is not contiguous!", RuntimeWarning)
                 input_ = input_.contiguous()
-            output = QuantizedFunctionsCUDA.Quantize_forward(input_, input_low, input_range, levels)
+            output = QuantizedFunctionsCUDA.Quantize_forward(input_, input_low.cuda(), input_range, levels)
         else:
             output = QuantizedFunctionsCPU.Quantize_forward(input_, input_low, input_range, levels)
 
